e10d5459623b1c66581a47af77bf63625f561621
S-Ketan <ketan10923@gmail.com>
2024-12-27 14:59:09 +0100

    Refactor user posts API route for improved error handling; update profile page to manage user posts with edit and delete functionality; enhance PromptCard component with user profile navigation and copy prompt feature.

--- app/api/user/[id]/posts/route.js
+++ app/api/user/[id]/posts/route.js
@@ -1,17 +1,15 @@
-import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
-import { NextResponse } from "next/server";
+import { connectToDatabase} from "@utils/database";
 
-export const GET = async (request,{params}) => {
-  try {
-    await connectToDatabase();
-    const prompts = await Prompt.find({creator: params.id}).populate("creator");
-    console.log("Fetched Prompts with Populated Creator:", prompts);
-    return NextResponse.json(prompts, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Failed to fetch all Prompts" },
-      { status: 500 }
-    );
-  }
-};
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDatabase()
+        
+        const prompts = await Prompt.find({ creator: params.id }).populate("creator")
+       
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (error) {
+        console.log(error)
+        return new Response("Failed to fetch prompts created by user", { status: 500 })
+    }
+} 
