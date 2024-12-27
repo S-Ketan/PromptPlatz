b9e46c5918b087233f6609e86c24506a8799639b
S-Ketan <ketan10923@gmail.com>
2024-12-27 05:41:31 +0100

    Implement feed component with search functionality; add API route for fetching prompts and create PromptCardList component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return <div className="mt-16 prompt_layout">
+    {data.map((post) => (
+      <PromptCard
+        key={post._id}
+        post={post}
+        tag={post.tag}
+        user={post.user}
+        handleTagClick={handleTagClick}
+      />
+    ))}
+  </div>;
+};
 
 const Feed = () => {
-  return (
-    <div>
-      Feed
-    </div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([])
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
 
-export default Feed
+useEffect(() => {
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+    setPosts(data);
+  };
+  fetchPosts();
+}, []);
+
+  return (
+    <section className="feed">
+      <form className="rekative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
--- models/prompt.js
+++ models/prompt.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
-import { Schema , model ,models} from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const promptSchema = new Schema({
-    creator:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    prompt: {
-        type: String,
-        required: [true, "Prompt is required"],
-    },
-    tag :{
-        type: String,
-        required: [true, "Tag is required"],   
-    }
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required"],
+  },
 });
 
 export default models.Prompt || model("Prompt", promptSchema);
