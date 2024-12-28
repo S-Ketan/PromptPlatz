c89962a7b75231412bb1b6a8bef4892621faaf2e
S-Ketan <ketan10923@gmail.com>
2024-12-28 07:57:05 +0100

    Refactor UserProfile component to accept request parameter

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -5,7 +5,7 @@ import { useSearchParams } from "next/navigation";
 
 import Profile from "@components/Profile";
 
-const UserProfile = ({ params }) => {
+const UserProfile = (request,{ params }) => {
   const searchParams = useSearchParams();
   const userName = searchParams.get("name");
 
